588f5d7122cdcadf1f52d782864af5f07acea71c
sandeep <97426095+sandeep30052004@users.noreply.github.com>
2022-05-03 14:58:58 +0530

    #include<stdio.h> int main() { 	char str[10]; 	scanf("%[^ ]s",str); 	if(str[0]=='1' && str[1]=='2' && str[6]=='A') 	{ 		printf("00:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='1' && str[6]=='A') 	{ 		printf("01:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='2' && str[6]=='A') 	{ 		printf("02:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='3' && str[6]=='A') 	{ 		printf("03:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='4' && str[6]=='A') 	{ 		printf("04:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='5' && str[6]=='A') 	{ 		printf("05:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='6' && str[6]=='A') 	{ 		printf("06:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='7' && str[6]=='A') 	{ 		printf("07:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='8' && str[6]=='A') 	{ 		printf("08:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='9' && str[6]=='A') 	{ 		printf("09:%c%c",str[3],str[4]); 	} 	else if(str[0]=='1' && str[1]=='0' && str[6]=='A') 	{ 		printf("10:%c%c",str[3],str[4]); 	} 	else if(str[0]=='1' && str[1]=='1' && str[6]=='A') 	{ 		printf("11:%c%c",str[3],str[4]); 	} 	else if(str[0]=='1' && str[1]=='2' && str[6]=='P') 	{ 		printf("12:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='1' && str[6]=='P') 	{ 		printf("13:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='2' && str[6]=='P') 	{ 		printf("14:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='3' && str[6]=='P') 	{ 		printf("15:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='4' && str[6]=='P') 	{ 		printf("16:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='5' && str[6]=='P') 	{ 		printf("17:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='6' && str[6]=='P') 	{ 		printf("18:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='7' && str[6]=='P') 	{ 		printf("19:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='8' && str[6]=='P') 	{ 		printf("20:%c%c",str[3],str[4]); 	} 	else if(str[0]=='0' && str[1]=='9' && str[6]=='P') 	{ 		printf("21:%c%c",str[3],str[4]); 	} 	else if(str[0]=='1' && str[1]=='0' && str[6]=='P') 	{ 		printf("22:%c%c",str[3],str[4]); 	} 	else if(str[0]=='1' && str[1]=='1' && str[6]=='P') 	{ 		printf("23:%c%c",str[3],str[4]); 	} }

---
+++ Time_conversion_(12_to_24_hour_format).c
@@ -0,0 +1,103 @@
+#include<stdio.h>
+int main()
+{
+	char str[10];
+	scanf("%[^
+]s",str);
+	if(str[0]=='1' && str[1]=='2' && str[6]=='A')
+	{
+		printf("00:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='1' && str[6]=='A')
+	{
+		printf("01:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='2' && str[6]=='A')
+	{
+		printf("02:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='3' && str[6]=='A')
+	{
+		printf("03:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='4' && str[6]=='A')
+	{
+		printf("04:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='5' && str[6]=='A')
+	{
+		printf("05:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='6' && str[6]=='A')
+	{
+		printf("06:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='7' && str[6]=='A')
+	{
+		printf("07:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='8' && str[6]=='A')
+	{
+		printf("08:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='9' && str[6]=='A')
+	{
+		printf("09:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='1' && str[1]=='0' && str[6]=='A')
+	{
+		printf("10:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='1' && str[1]=='1' && str[6]=='A')
+	{
+		printf("11:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='1' && str[1]=='2' && str[6]=='P')
+	{
+		printf("12:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='1' && str[6]=='P')
+	{
+		printf("13:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='2' && str[6]=='P')
+	{
+		printf("14:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='3' && str[6]=='P')
+	{
+		printf("15:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='4' && str[6]=='P')
+	{
+		printf("16:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='5' && str[6]=='P')
+	{
+		printf("17:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='6' && str[6]=='P')
+	{
+		printf("18:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='7' && str[6]=='P')
+	{
+		printf("19:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='8' && str[6]=='P')
+	{
+		printf("20:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='0' && str[1]=='9' && str[6]=='P')
+	{
+		printf("21:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='1' && str[1]=='0' && str[6]=='P')
+	{
+		printf("22:%c%c",str[3],str[4]);
+	}
+	else if(str[0]=='1' && str[1]=='1' && str[6]=='P')
+	{
+		printf("23:%c%c",str[3],str[4]);
+	}
+}
